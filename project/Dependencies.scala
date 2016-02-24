08acb176e97a3bc2faeba6349cc2329b1ddcaa86
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-02-24 13:47:56 +0100

    Update lift-json to version 2.6.3, circe to version 0.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,10 @@ object Dependencies {
   val akkaStream   = "com.typesafe.akka" %% "akka-stream"   % "2.4.2"
   val sprayJson    = "io.spray"          %% "spray-json"    % "1.3.2"
   val playJson     = "com.typesafe.play" %% "play-json"     % "2.4.6"
-  val liftJson     = "net.liftweb"       %% "lift-json"     % "2.6.2"
-  val circeCore    = "io.circe"          %% "circe-core"    % "0.1.1"
-  val circeGeneric = "io.circe"          %% "circe-generic" % "0.1.1"
-  val circeJawn    = "io.circe"          %% "circe-jawn"    % "0.1.1"
+  val liftJson     = "net.liftweb"       %% "lift-json"     % "2.6.3"
+  val circeCore    = "io.circe"          %% "circe-core"    % "0.3.0"
+  val circeGeneric = "io.circe"          %% "circe-generic" % "0.3.0"
+  val circeJawn    = "io.circe"          %% "circe-jawn"    % "0.3.0"
   val scalaTest    = "org.scalatest"     %% "scalatest"     % "2.2.6"
 
   val connectorDeps = Seq(akkaStream, sprayJson % Provided, playJson % Provided, liftJson % Provided,
