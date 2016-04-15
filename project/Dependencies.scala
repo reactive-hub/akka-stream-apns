5c1f2fd03cafa67f44ed18d12560f5e3950eaef5
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-04-15 11:25:03 +0200

    Update play-json to version 2.5.2, circe to version 0.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,10 @@ object Dependencies {
   val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.0.CR7"
 
   val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.2"
-  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.1"
+  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.2"
   val liftJson          = "net.liftweb"       %% "lift-json"           % "2.6.3"
-  val circeParser       = "io.circe"          %% "circe-parser"        % "0.4.0"
-  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.4.0"
+  val circeParser       = "io.circe"          %% "circe-parser"        % "0.4.1"
+  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.4.1"
 
   val scalaTest         = "org.scalatest"     %% "scalatest"           % "2.2.6"
   val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.4"
