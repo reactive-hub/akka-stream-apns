41b769b9f03c5bc13b281e7d860e56e64ed248f2
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-05-05 10:56:56 +0200

    Update play-json to version 2.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.0.CR7"
 
   val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.2"
-  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.2"
+  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.3"
   val liftJson          = "net.liftweb"       %% "lift-json"           % "2.6.3"
   val circeParser       = "io.circe"          %% "circe-parser"        % "0.4.1"
   val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.4.1"
