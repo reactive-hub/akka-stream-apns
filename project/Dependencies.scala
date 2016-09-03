f6cf940daa58c8e4b2f0d93b2265c674c72959cf
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-09-03 09:14:34 +0200

    Update netty to version 4.1.5.Final, circe to version 0.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies {
   val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % "2.4.9"
-  val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.4.Final"
+  val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.5.Final"
 
   val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.2"
   val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.6"
   val liftJson          = "net.liftweb"       %% "lift-json"           % "2.6.3"
-  val circeParser       = "io.circe"          %% "circe-parser"        % "0.4.1"
-  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.4.1"
+  val circeParser       = "io.circe"          %% "circe-parser"        % "0.5.0"
+  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.5.0"
 
   val scalaTest         = "org.scalatest"     %% "scalatest"           % "3.0.0"
   val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.9"
