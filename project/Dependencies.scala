ca235a3a21a52ad98e4ee1f1873778cf5de0ef4a
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-10-16 17:51:13 +0200

    Update dependencies
    * sbt-jmh: 0.2.16
    * akka: 2.4.11
    * netty: 4.1.6.Final
    * play-json: 2.5.9
    * circe: 0.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 object Dependencies {
-  val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % "2.4.9"
-  val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.5.Final"
+  val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % "2.4.11"
+  val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.6.Final"
 
   val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.2"
-  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.6"
+  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.9"
   val liftJson          = "net.liftweb"       %% "lift-json"           % "2.6.3"
-  val circeParser       = "io.circe"          %% "circe-parser"        % "0.5.0"
-  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.5.0"
+  val circeParser       = "io.circe"          %% "circe-parser"        % "0.5.3"
+  val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.5.3"
 
   val scalaTest         = "org.scalatest"     %% "scalatest"           % "3.0.0"
-  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.9"
+  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.11"
 
   val connectorDeps = Seq(akkaStream, netty) ++
     Seq(sprayJson, playJson, liftJson, circeParser).map(_ % Provided) ++
