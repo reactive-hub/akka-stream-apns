cf6ea371cc6c57cbc71a1a6a17a2be0acd8b7055
Marcel Mojzis <marcel.mojzis@reactivehub.com>
2016-12-28 23:10:17 +0100

    Update dependencies
    * akka: 2.4.16
    * play-json: 2.5.10
    * sbt-jmh: 0.2.20
    * scalatest: 3.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 object Dependencies {
-  val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % "2.4.11"
+  val akkaStream        = "com.typesafe.akka" %% "akka-stream"         % "2.4.16"
   val netty             = "io.netty"          %  "netty-codec-http2"   % "4.1.6.Final"
 
   val sprayJson         = "io.spray"          %% "spray-json"          % "1.3.2"
-  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.9"
+  val playJson          = "com.typesafe.play" %% "play-json"           % "2.5.10"
   val liftJson          = "net.liftweb"       %% "lift-json"           % "2.6.3"
   val circeParser       = "io.circe"          %% "circe-parser"        % "0.5.3"
   val circeGeneric      = "io.circe"          %% "circe-generic"       % "0.5.3"
 
-  val scalaTest         = "org.scalatest"     %% "scalatest"           % "3.0.0"
-  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.11"
+  val scalaTest         = "org.scalatest"     %% "scalatest"           % "3.0.1"
+  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % "2.4.16"
 
   val connectorDeps = Seq(akkaStream, netty) ++
     Seq(sprayJson, playJson, liftJson, circeParser).map(_ % Provided) ++
